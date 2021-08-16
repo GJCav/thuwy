f1b7a3c433fd126d4485ac354351562029c4948a
grw20 <g@grw20.cn>
2021-08-16 11:02:13 +0800

    feat(static): 文件上传主体功能

---
+++ static/config_example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    endPoint: 'DO_NOT_USE_LOCAL_ADDRESS',
+    endPointPort: 80,
+    accessKey: 'YOUR_ACCESS_KEY',
+    secretKey: 'YOUR_SECRET_KEY',
+    targetBucket: 'BUCKET_NAME',
+    listenPort: 8080,
+}
