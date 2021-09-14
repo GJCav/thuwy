6009dcb6e841057c531d5a1bdc6d9f7ec4e544aa
grw20 <g@grw20.cn>
2021-09-14 16:42:31 +0800

    refactor(webadmin): 修改config方式为环境变量

---
+++ webadmin/src/config.js
@@ -0,0 +1,5 @@
+export default {
+  WSAddr: process.env.VUE_APP_WS_ADDR,
+  ServerAddr: process.env.VUE_APP_SERVER_ADDR,
+  BackAPIAddr: process.env.VUE_APP_BACKAPI_ADDR
+};
--- webadmin/src/config_example.js
+++
@@ -1,5 +0,0 @@
-export default {
-  WSAddr: '',
-  ServerAddr: '',
-  BackAPIAddr: ''
-};
