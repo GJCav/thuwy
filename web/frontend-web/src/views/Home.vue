9187edf7e96345d5e564774d69d20bc9d2e97b55
grw20 <g@grw20.cn>
2021-09-16 19:45:04 +0800

    chore(web): web-frontend更改首页图标

--- web/frontend-web/src/views/Home.vue
+++ web/frontend-web/src/views/Home.vue
@@ -4,15 +4,17 @@
     cols="12"
     sm="6"
     offset-sm="3"
-    lg="4"
-    offset-lg="4"
+    xl="4"
+    offset-xl="4"
   >
     <v-row class="justify-center">
-      <v-img :src="require('@/assets/wy_blue.png')" />
+      <v-col cols="8">
+        <v-img :src="require('@/assets/wy_color.png')"></v-img>
+      </v-col>
     </v-row>
-    <h1>微未央（管理端）</h1>
     <br />
-    <v-card color="blue lighten-5" class="rounded" elevation="12">
+
+    <v-card color="blue lighten-5" class="rounded" elevation="12" ripple>
       <v-list-item two-line>
         <v-list-item-content v-if="user === null">
           <v-list-item-title class="text-h5">未登录</v-list-item-title>
