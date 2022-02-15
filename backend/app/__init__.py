5de0fa26893be6825c72310d5818813a8cb5b32c
Jiaming Gu <micojcav@outlook.com>
2022-02-15 11:30:41 +0800

    Revert "Backend zyx"

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -26,7 +26,6 @@ from .item import itemRouter
 from .reservation import rsvRouter
 from .advice import adviceRouter
 from .carousel import carouselRouter
-from .congyou import congyouRouter
 from .issue import issueRouter
 
 app.register_blueprint(authRouter)
@@ -34,7 +33,6 @@ app.register_blueprint(itemRouter)
 app.register_blueprint(rsvRouter)
 app.register_blueprint(adviceRouter)
 app.register_blueprint(carouselRouter)
-app.register_blueprint(congyouRouter)
 app.register_blueprint(issueRouter)
 
 Models.init_db(app)
--- backend/app/congyou/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-congyouRouter = Blueprint("congyou", __name__)
-
-from . import api as congyou_api
