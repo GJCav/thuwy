14633be6996fa6e732e866d67561a7f01667660a
Chris Zuo <zuojw21@mails.tsinghua.edu.cn>
2022-02-26 20:13:41 +0800

    feat: dayi-issue adds request for detailed info

--- frontend/pages/dayi/issue/issue.vue
+++ frontend/pages/dayi/issue/issue.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="col-container" style="align-items: flex-start; padding-bottom: 50rpx; background-color: #fffff9;">
-		<weiyang-background style="opacity: 0.4;"/>
+		<weiyang-background style="opacity: 0.4;" />
 		<view class="header">
 			<!-- 页眉 -->
 			<view class="title">
@@ -17,10 +17,10 @@
 		</view> <!-- .header -->
 		<view class="bubbles-container">
 			<!-- 所有气泡的容器 -->
-			<view 
-				:class="{'row-container': (bubble.tags.indexOf('#teacher') > -1), 'row-reverse-container':(bubble.tags.indexOf('#student') > -1)}"
+			<view
+				:class="{'row-container': (bubble.tags.indexOf('#teacher') > -1), 'row-reverse-container':(bubble.tags.indexOf('#teacher') === -1)}"
 				style="justify-content: flex-start; align-items: flex-start;width: 100%; padding-right: 50rpx; margin-top: 20rpx; "
-				v-for="bubble in bubbles">
+				v-for="bubble in bubbles" :key="bubble.id">
 				<!-- 每一个发言 -->
 				<image class="avatar" :src="avatarurl" />
 				<view style="padding-left: 0rpx; flex-grow: 1;">
@@ -31,12 +31,15 @@
 						<!-- <text style="margin-left: 10rpx;">最后更新于{{timeString(bubble.last_modified_at)}}</text> -->
 					</view>
 					<view
-						:class="{'bubble-teacher': (bubble.tags.indexOf('#teacher') > -1), 'bubble-student': (bubble.tags.indexOf('#student') > -1)}"
-					>
+						:class="{'bubble-teacher': (bubble.tags.indexOf('#teacher') > -1), 'bubble-student': (bubble.tags.indexOf('#teacher') === -1)}">
 						<!-- content -->
-						{{bubble.content.text}}
+						<text>{{bubble.content.text}}</text>
+						<image 
+							v-for="url in bubble.content.urls" :src="url" @click="clickImg(url)"
+							style="width: 95%;"
+							mode="widthFix"
+						></image>
 					</view>
-					<!-- <image v-for="url in " :url="bubble.content.picurl"/> -->
 				</view>
 			</view> <!-- .bubble -->
 		</view>
@@ -44,75 +47,67 @@
 </template>
 
 <script>
+	const app = getApp();
 	export default {
 		data() {
 			return {
+				admin: null,
 				avatarurl: '../../../static/main/setting/avatar.png',
 				bubbles: [{
-						id: 2,
-						title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
-						author: "000000",
-						date: 1645789960264,
-						last_modified_at: 0,
-						status: "open",
-						visibility: "public",
-						tags: ["#teacher", "GPA"],
-						reply_to: 1,
-						root_id: 1,
-						content: {
-							text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus justo et tortor iaculis, suscipit luctus magna accumsan. Integer aliquet volutpat tellus sit amet imperdiet. Nunc laoreet sem ac diam lacinia, nec volutpat massa tristique.",
-							picurl: ["", ""],
-						},
+					id: 2,
+					title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
+					author: "000000",
+					date: 1645789960264,
+					last_modified_at: 0,
+					status: "open",
+					visibility: "public",
+					tags: ["#teacher", "GPA"],
+					reply_to: 1,
+					root_id: 1,
+					content: {
+						text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus justo et tortor iaculis, suscipit luctus magna accumsan. Integer aliquet volutpat tellus sit amet imperdiet. Nunc laoreet sem ac diam lacinia, nec volutpat massa tristique.",
+						urls: ["https://dev-static.thuwy.top/image/2022/02/26/1645859133490_YhjbzUBU2W74230284f448f64339ec6ecc24d925972f.jpeg",
+							""
+						],
 					},
-					{
-						id: 2,
-						title: "test",
-						author: "000000",
-						date: 1645789960264,
-						last_modified_at: 0,
-						status: "open",
-						visibility: "public",
-						tags: ["#student", "GPA"],
-						reply_to: 1,
-						root_id: 1,
-						content: {
-							text: "Duis sed mauris eu urna faucibus rhoncus. Praesent vulputate, nunc id maximus vulputate, metus purus finibus lectus, interdum accumsan dui erat ut odio. Sed vitae nisl egestas lorem varius tincidunt in et urna. Nulla lacinia sodales metus, eget consectetur diam pharetra sed.",
-							picurl: ["", ""],
-						},
-					},
-					{
-							id: 2,
-							title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit?",
-							author: "000000",
-							date: 1645789960264,
-							last_modified_at: 0,
-							status: "open",
-							visibility: "public",
-							tags: ["#teacher", "GPA"],
-							reply_to: 1,
-							root_id: 1,
-							content: {
-								text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus justo et tortor iaculis, suscipit luctus magna accumsan. Integer aliquet volutpat tellus sit amet imperdiet. Nunc laoreet sem ac diam lacinia, nec volutpat massa tristique.",
-								picurl: ["", ""],
-							},
-						},
-				],
+				}, ],
 			};
 		},
 		computed: {
 			timeString() {
 				return (dateInt) => {
 					let date = new Date(dateInt);
-					return `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日 ${date.getHours()}:${date.getMinutes()}`;
+					return `${date.getFullYear()}年${date.getMonth()}月${date.getDate()}日 ${date.getHours()}:${(Array(2).join(0) + date.getMinutes()).slice(-2)}`;
 				}
 			},
 		},
+		methods: {
+			clickImg(url) {
+				wx.previewImage({
+					urls: [url], //需要预览的图片http链接列表，多张的时候，url直接写在后面就行了
+					current: '', // 当前显示图片的http链接，默认是第一个
+					success: function(res) {},
+					fail: function(res) {},
+					complete: function(res) {},
+				})
+			},
+		},
 		onLoad(e) {
-			this.admin=e.admin
+			this.admin = e.admin;
 			console.log(e.id)
 			// 根据e.id获取详细信息
-			
-		}
+			uni.request({
+				header: {
+					'content-type': 'application/json; charset=utf-8',
+					'cookie': wx.getStorageSync('cookie'),
+				},
+				url: app.globalData.url.backend + `/issue/${e.id}/`,
+				method: 'GET',
+			}).then(res => {
+				console.log(res);
+				this.bubbles = res.data.issues.slice();
+			})
+		},
 	}
 </script>
 
@@ -191,7 +186,7 @@
 		width: 0;
 		height: 0;
 		border: 20rpx solid transparent;
-		border-right-color: #00aabb;
+		border-right-color: #112C9A;
 		border-left: 0;
 		border-bottom: 0;
 		margin-top: -10rpx;
