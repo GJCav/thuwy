af802f90b192b08f125ee42b53af69a700a20545
JCav <micojcav@outlook.com>
2022-07-24 00:32:00 +0800

    devops(backend): 更新后端部署配置与文档、完全移除 nodejs 后端

--- web/backend-web/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    endPoint: process.env.ENDPOINT,
-    endPointPort: Number(process.env.ENDPOINT_PORT),
-    frontendOrigin: process.env.FRONTEND_ORIGIN,
-    targetBucket: process.env.BUCKET,
-    accessKey: process.env.ACCESS_KEY,
-    secretKey: process.env.SECRET_KEY,
-    listenPort: Number(process.env.PORT),
-}
