f5a39e7b89bc6268d078d52aee3cb00ec96595da
JCav <micojcav@outlook.com>
2022-08-24 01:32:01 +0800

    feat(webadmin): 实现用户组管理页面
    * 改进1：用户选择组件
    * 改进2：权限选择组件

--- web/webadmin/src/backend-api/auth.js
+++ web/webadmin/src/backend-api/auth.js
@@ -1,6 +1,11 @@
 import * as base from "./url"
 import urlcat from "urlcat";
 
+//////////////////////////////////////////////////////////
+//
+// 用户管理
+//
+
 export const fetchMyProfile = async ({ session }) => {
   const url = base.api("/profile/");
   const res = await fetch(url, {
@@ -32,6 +37,12 @@ export const fetchUserList = async ({ session, clazz, p, name }) => {
   const json = await res.json()
   return json
 }
+
+
+//////////////////////////////////////////////////////////
+//
+// 用户权限管理
+//
 
 export const getAllPrivileges = (privilege_info) => {
   let privilegeSet = []
@@ -89,4 +100,133 @@ export const delUserScope = async ({ session, name, openid }) => {
     throw new Error(`delUserScope: HTTP error: ${res.status}`)
   }
   return await res.json();
+}
+
+
+//////////////////////////////////////////////////////////
+//
+// 用户组管理
+//
+
+export const fetchGroupList = async ({ session, regex, p }) => {
+  const url = urlcat(base.API_URL + "/auth/group/", { regex, p })
+  const res = await fetch(url, { headers: { session }})
+  if (res.status !== 200) {
+    throw new Error(`fetchGroupList: HTTP error: ${res.status}`);
+  }
+  return await res.json();
+}
+
+
+export const delGroup = async ({ session, name }) => {
+  const url = urlcat(base.API_URL + "/auth/group/:name/", { name })
+  const res = await fetch(url, {
+    method: "delete",
+    headers: { session }
+  })
+  if (res.status !== 200) {
+    throw new Error(`delGroup: HTTP error: ${res.status}`);
+  }
+  return await res.json();
+}
+
+
+export const addGroup = async ({ session, name, expire_at }) => {
+  const url = base.api("/auth/group/")
+  const res = await fetch(url, {
+    method: "post",
+    headers: {
+      session,
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({ name, expire_at })
+  })
+  if (res.status !== 200) {
+    throw new Error(`addGroup: HTTP error: ${res.status}`);
+  }
+  return await res.json();
+}
+
+
+export const fetchGroupInfo = async ({ session, name }) => {
+  const url = urlcat(base.api("/auth/group/:name/"), { name })
+  const res = await fetch(url, { headers: { session }})
+  if (res.status !== 200) {
+    throw new Error(`fetchGroupInfo: HTTP error: ${res.status}`);
+  }
+  return await res.json();
+} 
+
+
+export const addGroupMember = async ({ session, openid, group_name }) => {
+  const url = urlcat(
+    base.api("/auth/group/:group_name/member/"),
+    { group_name }
+  )
+  const res = await fetch(url, {
+    method: 'post',
+    headers: {
+      session,
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({ openid })
+  })
+  if (res.status !== 200) {
+    throw new Error(`addGroupMember: HTTP error: ${res.status}`);
+  }
+  return await res.json()
+}
+
+
+export const delGroupMember = async ({ session, openid, group_name}) => {
+  const url = urlcat(
+    base.api("/auth/group/:group_name/member/:openid/"),
+    { group_name, openid }
+  )
+  const res = await fetch(url, {
+    method: 'delete',
+    headers: { session },
+  })
+  if (res.status !== 200) {
+    throw new Error(`delGroupMember: HTTP error: ${res.status}`);
+  }
+  return await res.json()
+}
+
+
+export const addGroupScope = async ({ session, scope, group_name }) => {
+  const url = urlcat(
+    base.api("/auth/group/:group_name/scope/"),
+    { group_name }
+  )
+  const res = await fetch(url, {
+    method: 'post',
+    headers: {
+      session,
+      "content-type": 'application/json'
+    },
+    body: JSON.stringify({ scope })
+  })
+  if (res.status !== 200) {
+    throw new Error(`addGroupScope: HTTP error: ${res.status}`);
+  }
+
+  return await res.json();
+}
+
+
+export const delGroupScope = async ({ session, scope, group_name }) => {
+  const url = urlcat(
+    base.api("/auth/group/:group_name/scope/:scope/"), {
+      group_name, scope
+    }
+  );
+  const res =  await fetch(url, {
+    method: 'delete',
+    headers: { session }
+  })
+  if (res.status !== 200) {
+    throw new Error(`delGroupScope: HTTP error: ${res.status}`);
+  }
+  return await res.json()
 }
--- web/webadmin/src/backend-api/url.js
+++ web/webadmin/src/backend-api/url.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://thuwy-dev.gjm20.top";
 
+
 export function api(path) { return this.API_URL + path; }
